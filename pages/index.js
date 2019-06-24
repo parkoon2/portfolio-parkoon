d70113219221f94de0f490b58e86c8128ef6795a
parkoon <par_koon@naver.com>
2019-06-24 12:32:48 +0900

    getInitialProps + async

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,29 @@
 import Header from '../components/shared/Header'
 import BaseLayout from '../components/layouts/BaseLayout'
+import axios from 'axios'
 
-const Index = () => {
+const Index = props => {
+  console.log('========= LOG START =======')
+  console.log(props)
+  console.log('========= LOG END =========')
+
   return (
     <BaseLayout>
-      <h1>Index page </h1>
+      <h1>{JSON.stringify(props)} </h1>
     </BaseLayout>
   )
+}
+
+Index.getInitialProps = async () => {
+  let res
+  try {
+    res = await axios.get('https://jsonplaceholder.typicode.com/todos/1')
+  } catch (err) {
+    console.error(err)
+  }
+  return {
+    initialData: [1, 2, 3, 4],
+    todo: res.data
+  }
 }
 export default Index
