124033147a2dc8b9e568fceaefa74930c463ae61
parkoon <par_koon@naver.com>
2019-07-05 16:14:42 +0900

    70. More Input Types

--- components/portfolios/PorfolioCreateForm.js
+++ components/portfolios/PorfolioCreateForm.js
@@ -1,20 +1,17 @@
 import React from 'react'
 
 export default class PortfolioCreateForm extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = { value: '' }
-
-    this.handleChange = this.handleChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
+  state = {
+    title: '',
+    description: '',
+    language: ''
   }
 
-  handleChange(event) {
-    this.setState({ value: event.target.value })
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value })
   }
-
-  handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.value)
+  handleSubmit = event => {
+    console.log('A name was submitted: ', this.state)
     event.preventDefault()
   }
 
@@ -22,12 +19,36 @@ export default class PortfolioCreateForm extends React.Component {
     return (
       <form onSubmit={this.handleSubmit}>
         <label>
-          Name:
+          title:
           <input
+            name="title"
             type="text"
-            value={this.state.value}
+            value={this.state.title}
             onChange={this.handleChange}
           />
+        </label>
+        <label>
+          description:
+          <textarea
+            name="description"
+            type="text"
+            value={this.state.description}
+            onChange={this.handleChange}
+          />
+        </label>
+        <label>
+          language:
+          <select
+            name="language"
+            value={this.state.languate}
+            onChange={this.handleChange}
+          >
+            <option value="javascript">Javascript</option>
+            <option value="react">React</option>
+            <option value="react-native">React Native</option>
+            <option value="mongodb">MongoDB</option>
+            <option value="webrtc">WebRTC</option>
+          </select>
         </label>
         <input type="submit" value="Submit" />
       </form>
