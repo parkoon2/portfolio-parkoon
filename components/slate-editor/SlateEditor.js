4a872986c06b246f4317d2f8b4b3bdbb37f082a7
parkoon <par_koon@naver.com>
2019-07-11 11:41:20 +0900

    109. Key event handler

--- components/slate-editor/SlateEditor.js
+++ components/slate-editor/SlateEditor.js
@@ -28,8 +28,25 @@ export default class SlateEditor extends React.Component {
     this.setState({ value })
   }
 
+  onKeyDown = (event, editor, next) => {
+    // Return with no changes if the keypress is not '&'
+    if (event.key !== '&') return next()
+
+    // Prevent the ampersand character from being inserted.
+    event.preventDefault()
+
+    // Change the value by inserting 'and' at the cursor's position.
+    editor.insertText('and')
+  }
+
   // Render the editor.
   render() {
-    return <Editor value={this.state.value} onChange={this.onChange} />
+    return (
+      <Editor
+        value={this.state.value}
+        onChange={this.onChange}
+        onKeyDown={this.onKeyDown}
+      />
+    )
   }
 }
