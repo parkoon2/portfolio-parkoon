944de6b27dfd33c669017a8680777bc93ff5fb12
parkoon <par_koon@naver.com>
2019-07-12 09:38:37 +0900

    118. create blog schema

--- server/models/blog.js
+++ server/models/blog.js
@@ -3,7 +3,14 @@ const Schema = mongoose.Schema
 
 const blogSchema = new Schema({
   userId: { type: String, required: true },
-  slug: { type: String, unique: true, sparse: true }
+  slug: { type: String, unique: true, sparse: true }, // what is spase...?
+  title: { type: String, required: true, maxlength: 96 },
+  subTitle: { type: String, required: true },
+  story: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+  status: { type: String, default: 'draft' },
+  author: { type: String, required: true }
 })
 
 module.exports = mongoose.model('Blog', blogSchema)
