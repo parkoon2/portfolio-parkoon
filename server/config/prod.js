5494d222264592e68c57cda1a3032e29c4dab2c9
parkoon <par_koon@naver.com>
2019-07-17 16:38:24 +0900

    174. Mongo DB & Auth0 Production Setup

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,5 +1,4 @@
 // ADD HERE VALURES BEFORE DEPLOY
 module.exports = {
-  DB_URI:
-    'mongodb+srv://parkoon:1234@parkoon-ttre7.mongodb.net/parkoon-portfolio?retryWrites=true&w=majority'
+  DB_URI: process.env.DB_URI
 }
