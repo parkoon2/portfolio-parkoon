9b7d213cd6686b368ac0485ced123ec60201303f
parkoon <par_koon@naver.com>
2019-07-17 17:22:42 +0900

    just for test

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,4 +1,6 @@
 // ADD HERE VALURES BEFORE DEPLOY
 module.exports = {
-  DB_URI: process.env.DB_URI
+  // DB_URI: process.env.DB_URI
+  DB_URI:
+    'mongodb+srv://parkoon:1234@parkoon-ttre7.mongodb.net/parkoon-portfolio?retryWrites=true&w=majority'
 }
