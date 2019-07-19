562f890746094068e61b8a226b7390776c7516f1
parkoon <par_koon@naver.com>
2019-07-19 12:55:34 +0900

    Decrease image sizes, load fonts on clinet, compress final js

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,6 +1,4 @@
 // ADD HERE VALURES BEFORE DEPLOY
 module.exports = {
-  // DB_URI: process.env.DB_URI
-  DB_URI:
-    'mongodb+srv://parkoon:1234@parkoon-ttre7.mongodb.net/parkoon-portfolio?retryWrites=true&w=majority'
+  DB_URI: process.env.DB_URI
 }
